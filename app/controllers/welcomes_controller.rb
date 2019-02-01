5c0d6613eb3df26e29b1f18611a07d1991023924
kirbycampbell <jkirbycampbell@gmail.com>
2019-02-01 10:10:23 -0800

    add heroku

--- app/controllers/welcomes_controller.rb
+++ app/controllers/welcomes_controller.rb
@@ -1,5 +1,5 @@
 class WelcomesController < ApplicationController
-  before_action :set_welcome, only: [:show, :edit, :update, :destroy]
+  before_action :set_welcome, only: [:index, :show, :edit, :update, :destroy]
 
   # GET /welcomes
   # GET /welcomes.json
